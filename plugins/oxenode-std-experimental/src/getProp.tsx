68f0fb9940a86eade717a140d53fc4aa1393d12f
vlev <matiasvlevi@gmail.com>
2023-11-05 13:28:32 -0500

    Added setProp & getProp in experimental

---
+++ plugins/oxenode-std-experimental/src/getProp.tsx
@@ -0,0 +1,39 @@
+import { ContentProps, FetchProps, port } from '@oxenode/core'
+import { TextInput } from '@oxenode/ui'
+
+export const Name = "get"
+
+const spanStyle = {
+    fontSize: '44px',
+    padding: '0 0.7rem'
+}
+
+export default function Content({ nodeId }: ContentProps) {
+    return <div style={{
+        display:'inline-flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignContent: 'center',
+        margin: 0
+    }}>
+        <span style={spanStyle}>&#123;&#123;</span>
+        <TextInput
+            style={{
+                fontSize: '32px',
+                maxWidth: '14rem',
+                margin: 0
+            }}
+            nodeId={nodeId}
+            name='value'
+        />
+        <span style={spanStyle}>&#125;&#125;</span>
+    </div>
+}
+
+export const ports = [
+  port.input().type("data"),
+  port
+    .output()
+    .type(["string", "number", "data"])
+    .onFetch(({ state, inputs }: FetchProps) => inputs[0][state.value]),
+];
---
+++ plugins/oxenode-std-experimental/src/setProp.tsx
@@ -0,0 +1,43 @@
+import { ContentProps, FetchProps, port } from "@oxenode/core";
+import { TextInput } from "@oxenode/ui";
+
+export const Name = "set";
+
+export default function Content({ nodeId }: ContentProps) {
+  return (
+    <div
+      style={{
+        display: "inline-flex",
+        flexDirection: "row",
+        alignItems: "center",
+        alignContent: "center",
+        margin: 0,
+      }}
+    >
+      <TextInput
+        style={{
+          fontSize: "28px",
+          maxWidth: "14rem",
+          margin: 0,
+        }}
+        nodeId={nodeId}
+        name="key"
+      />
+    </div>
+  );
+}
+
+export const ports = [
+  port.input().type(["string", "number", "data"]).label("value"),
+  port.input().type("data").label("object"),
+  port
+    .output()
+    .type("data")
+    .onFetch(({ inputs, state }: FetchProps) => {
+      const ans = { ...(inputs.object || {}) };
+
+      ans[state.key] = inputs.value;
+
+      return ans;
+    }),
+];
