d98fa92be5b419903457cbb06632c526497615cb
vlev <matiasvlevi@gmail.com>
2023-12-29 14:47:16 -0500

    fetch use error component

--- plugins/oxenode-std-http/src/fetch.tsx
+++ plugins/oxenode-std-http/src/fetch.tsx
@@ -1,5 +1,5 @@
 import { ContentProps, onFetchProps, TriggerProps, port } from "@oxenode/core";
-import { Select } from "@oxenode/ui";
+import { Select, ErrorMessage } from "@oxenode/ui";
 
 const methodWithBodies = [
   "POST",
@@ -9,11 +9,11 @@ const methodWithBodies = [
 
 export const Name = "Fetch";
 
-export default function Content({ nodeId }: ContentProps) {
+export default function Content({ node }: ContentProps) {
   return (
     <>
       <h2>Fetch</h2>
-      <Select nodeId={nodeId} name="method" value="GET">
+      <Select nodeId={node.id} name="method" value="GET">
         <option value="GET">GET</option>
         <option value="POST">POST</option>
         <option value="PUT">PUT</option>
@@ -21,36 +21,52 @@ export default function Content({ nodeId }: ContentProps) {
         <option value="DELETE">DELETE</option>
       </Select>
       <br/>
-      <Select nodeId={nodeId} name="parseMode" value="json">
+      <Select nodeId={node.id} name="parseMode" value="json">
         <option value="json">json</option>
         <option value="text">text</option>
         <option value="blob">blob</option>
         <option value="arrayBuffer">arrayBuffer</option>
         <option value="clone">clone</option>
         <option value="formData">formData</option>
-      </Select>
+      </Select>      
+      <br/>
+
+      <ErrorMessage name="errr" nodeId={node.id}/>
     </>
   );
 }
 
 export async function Trigger({ 
+  node,
   state: { method, parseMode },
   inputs: { url, options, body },
   controller
 }: TriggerProps) {
-
+  
   options = { method, ...options };
 
   if (methodWithBodies.includes(method)) {
     options.body = JSON.stringify(body || {});
   }
 
+  // Remove previous error
+  if (node.State.err) delete node.State.err;
+  controller.update(node);
+  
   fetch(url, options)
     .then((res: any) => (res as Record<string, () => {}>)[parseMode]())
     .then((value: Record<string, any>) => {
       controller.setCache("return", value);
+
+      controller.update(node);
       controller.trigger(0);
-    });
+    })
+    .catch(error => {
+      node.State.err = `${error}`;
+
+      controller.update(node);
+      controller.trigger(0);
+    })
 }
 
 export const ports = [
