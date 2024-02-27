15a0ce2f8750ab8fba32c5a93e66bed7c20409f1
vlev <matiasvlevi@gmail.com>
2024-02-27 02:28:32 -0500

    better process spawning

--- mono.js
+++ mono.js
@@ -1,31 +1,56 @@
 const { spawn } = require('node:child_process');
 const fs = require('node:fs');
 
-(() => {
+const PACKAGE_MANAGER = 'npm';
+const PROC_COUNT = 4;
+
+/**
+ * Spawn `PROC_COUNT` processes with the `--prefix=` command for all modules
+ */
+(async () => {
     const command = process.argv[2] || 'run';
     const script = process.argv[3] || '';
-    
+
     const pluginDir = './plugins'
     const plugins = fs.readdirSync(pluginDir);
-    
-    for (let plugin of plugins) {
-
-        const proc = spawn(
-            `npm`,
-            [
+    let procs = [];
+    while (plugins.length > 0) {
+        for (let i = 0; (i < PROC_COUNT) && plugins[i]; i++) {
+            const args = [
                 `${command}`,
                 `${script}`,
-                `--prefix=${pluginDir}/${plugin}`
+                `--prefix=${pluginDir}/${plugins[i]}`
             ]
-        );
+            procs.push({
+                command: [PACKAGE_MANAGER, ...args].join(' '),
+                proc: new Promise((resolve, reject) => {
+                    const proc = spawn(PACKAGE_MANAGER, args);
+            
+                    proc.stderr.on('data', (data) => {
+                        reject(data);
+                    });
+            
+                    proc.stdout.on('data', (data) => {
+                        process.stdout.write(data);
+                    });
+            
+                    //proc.on('close', (data) => resolve(data));
+                    proc.on('exit', (data) => resolve(data))
+                })
+            });
+        }
+        process.stdout.write('\x1b[32m\n');
+        procs.forEach(proc => {
+            process.stdout.write(proc.command + '\n');
+        })
+        process.stdout.write('\x1b[0m\n');
 
-        proc.stderr.on('data', (data) => {
-            process.stderr.write(data);
+        await Promise.all(procs.map(p => p.proc)).catch(e => {
+            process.stderr.write(e);
         });
 
-        proc.stdout.on('data', (data) => {
-            process.stdout.write(data);
-        })
-
+        procs = [];
+        for (let i = 0; i < PROC_COUNT; i++) 
+            plugins.shift();
     }
 })();
